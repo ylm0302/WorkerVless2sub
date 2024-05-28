e4f99747c241e37388c4ddb7ca70c30950bb59ac
ylm0302 <525668066@qq.com>
2024-05-28 10:10:17 +0800

    Update _worker.js
    移除自定义域名的处理

--- _worker.js
+++ _worker.js
@@ -606,7 +606,7 @@ export default {
 				if(proxyhosts && (host.includes('.workers.dev') || host.includes('pages.dev'))) {
 					最终路径 = `/${host}${path}`;
 					伪装域名 = proxyhosts[Math.floor(Math.random() * proxyhosts.length)];
-					节点备注 = `${EndPS} 已启用临时域名中转服务，请尽快绑定自定义域！`;
+					节点备注 = `${EndPS} `;
 					sni = 伪装域名;
 				}
 
